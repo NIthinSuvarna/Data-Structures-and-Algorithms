ab739d459da46357b3be94c4b27995a55e746ec3
Nithin Suvarna <nithinsuvarna17@gmail.com>
2020-09-02 04:55:05 +0000

    Added program to demo static and global var behavior in recursive functions

---
+++ Recursion/StaticGlobalRecursion/StaticGlobal.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//int var = 0; //uncomment this line and comment static var initialisation to see global behaviour
+int recFun(int n)
+{
+    static int var = 0;
+    if (n > 0)
+    {
+        var++;
+        return recFun(n - 1) + var;
+    }
+    return 0;  //v v imp, if this return is missed line 21 will return junk value.
+}
+
+int main()
+{
+    int x = 5;
+    cout << "The value is:\t" << recFun(x);
+    return 0;
+}
